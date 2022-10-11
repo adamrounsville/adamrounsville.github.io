4e762ba4e8c7fe04ba71195c2dc9e1ccd814d8f4
Adam Rounsvill <adamjrounsville@gmail.com>
2022-10-11 00:04:18 -0600

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# GitHub Pages Repository
-My GitHub Pages repository to display content on my website ([adamrounsville.me](https://adamrounsville.me)) built using React, JavaScript, HTML, and CSS
---
+++ precache-manifest.e77db69d2cdb5a73cac5e176aae22246.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c249f4a94fd7a9fb03a12da1a16577db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0068566c3e4b615352a9",
+    "url": "/static/css/3.6667b5be.chunk.css"
+  },
+  {
+    "revision": "e2828b69a0df4c01c54d",
+    "url": "/static/css/4.6076b386.chunk.css"
+  },
+  {
+    "revision": "0db6481836b4a33a19ae",
+    "url": "/static/css/main.13e726fa.chunk.css"
+  },
+  {
+    "revision": "af213f4fe67dfe3fc188",
+    "url": "/static/js/2.1fc05f96.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.1fc05f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0068566c3e4b615352a9",
+    "url": "/static/js/3.f191c75a.chunk.js"
+  },
+  {
+    "revision": "e2828b69a0df4c01c54d",
+    "url": "/static/js/4.dd26b2bc.chunk.js"
+  },
+  {
+    "revision": "0db6481836b4a33a19ae",
+    "url": "/static/js/main.9d9560ca.chunk.js"
+  },
+  {
+    "revision": "1e1290e058a5099bc925",
+    "url": "/static/js/runtime-main.e77a64a4.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8c826474f2b497b1347266ca281ecece",
+    "url": "/static/media/byuLogo.8c826474.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "6d7ce63215010d2677fb76e093306d8b",
+    "url": "/static/media/developerActivity.6d7ce632.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  }
+]);
